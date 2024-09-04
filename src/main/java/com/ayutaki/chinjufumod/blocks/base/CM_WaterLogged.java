f15465ff9a33419aafc1aabc506d73a0c3242796
Alex Lee <islicy@outlook.com>
2024-09-04 11:21:25 -0400

    Use old CM_WaterLogged

--- src/main/java/com/ayutaki/chinjufumod/blocks/base/CM_WaterLogged.java
+++ src/main/java/com/ayutaki/chinjufumod/blocks/base/CM_WaterLogged.java
@@ -1,12 +1,8 @@
 package com.ayutaki.chinjufumod.blocks.base;
 
 import java.util.Optional;
-
-import javax.annotation.Nullable;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
@@ -21,53 +17,59 @@ import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 
+/* Liquid method only. */
 public class CM_WaterLogged extends Block implements SimpleWaterloggedBlock {
 
-	public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
-	
-	public CM_WaterLogged(BlockBehaviour.Properties properties) {
-		super(properties);
-	}
-	
-	/* Waterlogged */
-	@SuppressWarnings("deprecation")
-	public FluidState getFluidState(BlockState state) {
-		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-	
-	public boolean propagatesSkylightDown(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return !state.getValue(WATERLOGGED);
-	}
+  /* Property */
+  public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
 
-	public boolean canPlaceLiquid(@Nullable Player playerIn, BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
-		return fluid == Fluids.WATER;
-	} // fix 20.2
+  public CM_WaterLogged(BlockBehaviour.Properties properties) {
+    super(properties);
+  }
 
-	public boolean placeLiquid(LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
-		if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
-			if (!worldIn.isClientSide()) {
-				worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
-				worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn)); }
+  /* Waterlogged */
+  @SuppressWarnings("deprecation")
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+  }
 
-			return true;
-		} 
-		
-		else { return false; }
-	}
+  @Override
+  public boolean canPlaceLiquid(BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
+    return !state.getValue(BlockStateProperties.WATERLOGGED) && fluid == Fluids.WATER;
+  }
 
-	@Override
-	public ItemStack pickupBlock(@Nullable Player playerIn, LevelAccessor worldIn, BlockPos pos, BlockState state) {
-		if (state.getValue(BlockStateProperties.WATERLOGGED)) {
-			worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
-			
-			if (!state.canSurvive(worldIn, pos)) { worldIn.destroyBlock(pos, true); }
-			return new ItemStack(Items.WATER_BUCKET);
-		}
-		
-		else { return ItemStack.EMPTY; }
-	} // fix 20.2
+  @Override
+  public boolean placeLiquid(
+      LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
+    if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
+      if (!worldIn.isClientSide()) {
+        worldIn.setBlock(
+            pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
+        worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn));
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	public Optional<SoundEvent> getPickupSound() {
-		return Fluids.WATER.getPickupSound();
-	}
+  @Override
+  public ItemStack pickupBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+    if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+      worldIn.setBlock(
+          pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
+
+      if (!state.canSurvive(worldIn, pos)) {
+        worldIn.destroyBlock(pos, true);
+      }
+      return new ItemStack(Items.WATER_BUCKET);
+    } else {
+      return ItemStack.EMPTY;
+    }
+  }
+
+  @Override
+  public Optional<SoundEvent> getPickupSound() {
+    return Fluids.WATER.getPickupSound();
+  }
 }
