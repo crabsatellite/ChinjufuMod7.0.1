c4d705941a3925c4d43ea7d0c944fa272690ad83
Alex Lee <islicy@outlook.com>
2024-09-04 14:11:30 -0400

    Update Kit_Board for 1.20.1

--- src/main/java/com/ayutaki/chinjufumod/blocks/kitchen/Kit_Board.java
+++ src/main/java/com/ayutaki/chinjufumod/blocks/kitchen/Kit_Board.java
@@ -1,13 +1,10 @@
 package com.ayutaki.chinjufumod.blocks.kitchen;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.annotation.Nullable;
-
 import com.ayutaki.chinjufumod.gui.KitBoardMenu;
 import com.ayutaki.chinjufumod.handler.SoundEvents_CM;
-
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.Nullable;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -49,140 +46,181 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 /* CraftingTableBlock + facing Property */
 public class Kit_Board extends CraftingTableBlock implements SimpleWaterloggedBlock {
-	
-	private static final Component CONTAINER_TITLE = Component.translatable("container.crafting");
-	/* Property */
-	public static final DirectionProperty H_FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
-	public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
 
-	/* Collision */
-	protected static final VoxelShape TOP = Block.box(0.0D, 15.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-	
-	protected static final VoxelShape AABB_SOUTH = Shapes.or(TOP, Block.box(0.0D, 0.0D, 1.0D, 16.0D, 15.0D, 15.0D));
-	protected static final VoxelShape AABB_WEST = Shapes.or(TOP, Block.box(1.0D, 0.0D, 0.0D, 15.0D, 15.0D, 16.0D));
-	protected static final VoxelShape AABB_NORTH = Shapes.or(TOP, Block.box(0.0D, 0.0D, 1.0D, 16.0D, 15.0D, 15.0D));
-	protected static final VoxelShape AABB_EAST = Shapes.or(TOP, Block.box(1.0D, 0.0D, 0.0D, 15.0D, 15.0D, 16.0D));
+  private static final Component CONTAINER_TITLE = Component.translatable("container.crafting");
+  /* Property */
+  public static final DirectionProperty H_FACING =
+      DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
+  public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
 
-	public Kit_Board(BlockBehaviour.Properties properties) {
-		super(properties);
-		/** Default state **/
-		registerDefaultState(this.stateDefinition.any().setValue(H_FACING, Direction.NORTH)
-				.setValue(WATERLOGGED, Boolean.valueOf(false)));
-	}
-	
-	/* RightClick Action*/
-	@Override
-	public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player playerIn, InteractionHand hand, BlockHitResult hit) {
-		if (worldIn.isClientSide) { return InteractionResult.SUCCESS; }
-		
-		else {
-			/** Open UI. **/
-			playerIn.openMenu(state.getMenuProvider(worldIn, pos));
-			playerIn.awardStat(Stats.INTERACT_WITH_CRAFTING_TABLE);
-			
-			worldIn.playSound(null, pos, SoundEvents_CM.KITCHEN_CUT.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
-			return InteractionResult.CONSUME;
-		}
-	}
-	
-	@Override
-	public MenuProvider getMenuProvider(BlockState state, Level worldIn, BlockPos pos) {
-		return new SimpleMenuProvider((id, inventory, entity) -> {
-			return new KitBoardMenu(id, inventory, ContainerLevelAccess.create(worldIn, pos)); }, CONTAINER_TITLE);
-	}
-	
-	/* Gives a value when placed. +180 .getOpposite() */
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		Level worldIn = context.getLevel();
-		BlockPos pos = context.getClickedPos();
-		FluidState fluid = worldIn.getFluidState(pos);
-		
-		return this.defaultBlockState().setValue(WATERLOGGED, fluid.getType() == Fluids.WATER)
-				.setValue(H_FACING, context.getHorizontalDirection().getOpposite());
-	}
-	
-	/* HORIZONTAL Property */
-	@Override
-	public BlockState rotate(BlockState state, Rotation rotation) {
-		return state.setValue(H_FACING, rotation.rotate(state.getValue(H_FACING)));
-	}
+  /* Collision */
+  protected static final VoxelShape TOP = Block.box(0.0D, 15.0D, 0.0D, 16.0D, 16.0D, 16.0D);
 
-	@SuppressWarnings("deprecation")
-	public BlockState mirror(BlockState state, Mirror mirror) {
-		return state.rotate(mirror.getRotation(state.getValue(H_FACING)));
-	}
+  protected static final VoxelShape AABB_SOUTH =
+      Shapes.or(TOP, Block.box(0.0D, 0.0D, 1.0D, 16.0D, 15.0D, 15.0D));
+  protected static final VoxelShape AABB_WEST =
+      Shapes.or(TOP, Block.box(1.0D, 0.0D, 0.0D, 15.0D, 15.0D, 16.0D));
+  protected static final VoxelShape AABB_NORTH =
+      Shapes.or(TOP, Block.box(0.0D, 0.0D, 1.0D, 16.0D, 15.0D, 15.0D));
+  protected static final VoxelShape AABB_EAST =
+      Shapes.or(TOP, Block.box(1.0D, 0.0D, 0.0D, 15.0D, 15.0D, 16.0D));
 
-	/* Waterlogged */
-	@SuppressWarnings("deprecation")
-	public FluidState getFluidState(BlockState state) {
-		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-	
-	@Override
-	public boolean canPlaceLiquid(@Nullable Player playerIn, BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
-		return fluid == Fluids.WATER;
-	} // fix 20.2
-	
-	@Override
-	public boolean placeLiquid(LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
-		if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
-			if (!worldIn.isClientSide()) {
-				worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
-				worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn)); }
-			return true; }
-		
-		else { return false; }
-	}
-	
-	@Override
-	public ItemStack pickupBlock(@Nullable Player playerIn, LevelAccessor worldIn, BlockPos pos, BlockState state) {
-		if (state.getValue(BlockStateProperties.WATERLOGGED)) {
-			worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
-			
-			if (!state.canSurvive(worldIn, pos)) { worldIn.destroyBlock(pos, true); }
-			return new ItemStack(Items.WATER_BUCKET);
-		}
-		
-		else { return ItemStack.EMPTY; }
-	} // fix 20.2
-	
-	@Override
-	public Optional<SoundEvent> getPickupSound() {
-		return Fluids.WATER.getPickupSound();
-	}
-	
-	/* Update BlockState. */
-	@SuppressWarnings("deprecation")
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor worldIn, BlockPos pos, BlockPos facingPos) {
-		if (state.getValue(WATERLOGGED)) {
-			worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn)); }
-		
-		return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
-	}
-	
-	/* Create Blockstate */
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(H_FACING, WATERLOGGED);
-	}
+  public Kit_Board(BlockBehaviour.Properties properties) {
+    super(properties);
+    /** Default state * */
+    registerDefaultState(
+        this.stateDefinition
+            .any()
+            .setValue(H_FACING, Direction.NORTH)
+            .setValue(WATERLOGGED, Boolean.valueOf(false)));
+  }
 
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-		Direction direction = state.getValue(H_FACING);
+  /* RightClick Action*/
+  @Override
+  public InteractionResult use(
+      BlockState state,
+      Level worldIn,
+      BlockPos pos,
+      Player playerIn,
+      InteractionHand hand,
+      BlockHitResult hit) {
+    if (worldIn.isClientSide) {
+      return InteractionResult.SUCCESS;
+    } else {
+      /** Open UI. * */
+      playerIn.openMenu(state.getMenuProvider(worldIn, pos));
+      playerIn.awardStat(Stats.INTERACT_WITH_CRAFTING_TABLE);
 
-		switch (direction) {
-		case NORTH:
-		default: return AABB_NORTH;
-		case SOUTH: return AABB_SOUTH;
-		case WEST: return AABB_WEST;
-		case EAST: return AABB_EAST;
-		} // switch
-	}
-	
-	/* ToolTip */
-	@Override
-	public void appendHoverText(ItemStack stack, @Nullable BlockGetter worldIn, List<Component> tooltip, TooltipFlag tipFlag) {
-		tooltip.add(Component.translatable("tips.block_kit_board").withStyle(ChatFormatting.GRAY));
-	}
+      worldIn.playSound(
+          null, pos, SoundEvents_CM.KITCHEN_CUT.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
+      return InteractionResult.CONSUME;
+    }
+  }
+
+  @Override
+  public MenuProvider getMenuProvider(BlockState state, Level worldIn, BlockPos pos) {
+    return new SimpleMenuProvider(
+        (id, inventory, entity) -> {
+          return new KitBoardMenu(id, inventory, ContainerLevelAccess.create(worldIn, pos));
+        },
+        CONTAINER_TITLE);
+  }
+
+  /* Gives a value when placed. +180 .getOpposite() */
+  @Override
+  public BlockState getStateForPlacement(BlockPlaceContext context) {
+    Level worldIn = context.getLevel();
+    BlockPos pos = context.getClickedPos();
+    FluidState fluid = worldIn.getFluidState(pos);
+
+    return this.defaultBlockState()
+        .setValue(WATERLOGGED, fluid.getType() == Fluids.WATER)
+        .setValue(H_FACING, context.getHorizontalDirection().getOpposite());
+  }
+
+  /* HORIZONTAL Property */
+  @Override
+  public BlockState rotate(BlockState state, Rotation rotation) {
+    return state.setValue(H_FACING, rotation.rotate(state.getValue(H_FACING)));
+  }
+
+  @SuppressWarnings("deprecation")
+  public BlockState mirror(BlockState state, Mirror mirror) {
+    return state.rotate(mirror.getRotation(state.getValue(H_FACING)));
+  }
+
+  /* Waterlogged */
+  @SuppressWarnings("deprecation")
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+  }
+
+  @Override
+  public boolean canPlaceLiquid(BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
+    return !state.getValue(BlockStateProperties.WATERLOGGED) && fluid == Fluids.WATER;
+  }
+
+  @Override
+  public boolean placeLiquid(
+      LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
+    if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
+      if (!worldIn.isClientSide()) {
+        worldIn.setBlock(
+            pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
+        worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn));
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public ItemStack pickupBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+    if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+      worldIn.setBlock(
+          pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
+
+      if (!state.canSurvive(worldIn, pos)) {
+        worldIn.destroyBlock(pos, true);
+      }
+      return new ItemStack(Items.WATER_BUCKET);
+    } else {
+      return ItemStack.EMPTY;
+    }
+  }
+
+  @Override
+  public Optional<SoundEvent> getPickupSound() {
+    return Fluids.WATER.getPickupSound();
+  }
+
+  /* Update BlockState. */
+  @SuppressWarnings("deprecation")
+  public BlockState updateShape(
+      BlockState state,
+      Direction facing,
+      BlockState facingState,
+      LevelAccessor worldIn,
+      BlockPos pos,
+      BlockPos facingPos) {
+    if (state.getValue(WATERLOGGED)) {
+      worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+    }
+
+    return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
+  }
+
+  /* Create Blockstate */
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    builder.add(H_FACING, WATERLOGGED);
+  }
+
+  @Override
+  public VoxelShape getShape(
+      BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+    Direction direction = state.getValue(H_FACING);
+
+    switch (direction) {
+      case NORTH:
+      default:
+        return AABB_NORTH;
+      case SOUTH:
+        return AABB_SOUTH;
+      case WEST:
+        return AABB_WEST;
+      case EAST:
+        return AABB_EAST;
+    } // switch
+  }
+
+  /* ToolTip */
+  public void appendHoverText(
+      ItemStack stack,
+      @Nullable BlockGetter worldIn,
+      List<Component> tooltip,
+      TooltipFlag tipFlag) {
+    tooltip.add(Component.translatable("tips.block_kit_board").withStyle(ChatFormatting.GRAY));
+  }
 }
