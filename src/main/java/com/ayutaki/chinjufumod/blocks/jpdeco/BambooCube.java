70d984fb378806c0bf0c13e273ba022a8320095e
Alex Lee <islicy@outlook.com>
2024-09-04 14:10:01 -0400

    Update BambooCube for 1.20.1

--- src/main/java/com/ayutaki/chinjufumod/blocks/jpdeco/BambooCube.java
+++ src/main/java/com/ayutaki/chinjufumod/blocks/jpdeco/BambooCube.java
@@ -1,13 +1,9 @@
 package com.ayutaki.chinjufumod.blocks.jpdeco;
 
 import java.util.Optional;
-
-import javax.annotation.Nullable;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -28,89 +24,113 @@ import net.minecraft.world.level.material.Fluids;
 
 public class BambooCube extends RotatedPillarBlock implements SimpleWaterloggedBlock {
 
-	/* Property */
-	public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
-	
-	public BambooCube(BlockBehaviour.Properties properties) {
-		super(properties);
-		/** Default state **/
-		registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y)
-				.setValue(WATERLOGGED, Boolean.valueOf(false)));
-	}
-	
-	/* Gives a value when placed. +180 .getOpposite() */
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		Level worldIn = context.getLevel();
-		BlockPos pos = context.getClickedPos();
-		FluidState fluid = worldIn.getFluidState(pos);
+  /* Property */
+  public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
 
-		return this.defaultBlockState().setValue(AXIS, context.getClickedFace().getAxis())
-				.setValue(WATERLOGGED, Boolean.valueOf(fluid.getType() == Fluids.WATER));
-	}
-	
-	/* Waterlogged */
-	@SuppressWarnings("deprecation")
-	public FluidState getFluidState(BlockState state) {
-		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-	
-	@Override
-	public boolean canPlaceLiquid(@Nullable Player playerIn, BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
-		return fluid == Fluids.WATER;
-	} // fix 20.2
-	
-	@Override
-	public boolean placeLiquid(LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
-		if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
-			if (!worldIn.isClientSide()) {
-				worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
-				worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn)); }
-			return true; }
-		
-		else { return false; }
-	}
-	
-	@Override
-	public ItemStack pickupBlock(@Nullable Player playerIn, LevelAccessor worldIn, BlockPos pos, BlockState state) {
-		if (state.getValue(BlockStateProperties.WATERLOGGED)) {
-			worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
-			
-			if (!state.canSurvive(worldIn, pos)) { worldIn.destroyBlock(pos, true); }
-			return new ItemStack(Items.WATER_BUCKET);
-		}
-		
-		else { return ItemStack.EMPTY; }
-	} // fix 20.2
-	
-	@Override
-	public Optional<SoundEvent> getPickupSound() {
-		return Fluids.WATER.getPickupSound();
-	}
-	
-	/* Update BlockState. */
-	@SuppressWarnings("deprecation")
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor worldIn, BlockPos pos, BlockPos facingPos) {
-		if (state.getValue(WATERLOGGED)) {
-			worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn)); }
-		
-		return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
-	}
-	
-	/* Create Blockstate */
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(AXIS, WATERLOGGED);
-	}
-	
-	/* Flammable Block */
-	@Override
-	public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction direct) { return true; }
+  public BambooCube(BlockBehaviour.Properties properties) {
+    super(properties);
+    /** Default state * */
+    registerDefaultState(
+        this.stateDefinition
+            .any()
+            .setValue(AXIS, Direction.Axis.Y)
+            .setValue(WATERLOGGED, Boolean.valueOf(false)));
+  }
 
-	@Override
-	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction direct) { return 5; }
+  /* Gives a value when placed. +180 .getOpposite() */
+  @Override
+  public BlockState getStateForPlacement(BlockPlaceContext context) {
+    Level worldIn = context.getLevel();
+    BlockPos pos = context.getClickedPos();
+    FluidState fluid = worldIn.getFluidState(pos);
 
-	@Override
-	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction direct) { return 20; }
-	
+    return this.defaultBlockState()
+        .setValue(AXIS, context.getClickedFace().getAxis())
+        .setValue(WATERLOGGED, Boolean.valueOf(fluid.getType() == Fluids.WATER));
+  }
+
+  /* Waterlogged */
+  @SuppressWarnings("deprecation")
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+  }
+
+  @Override
+  public boolean canPlaceLiquid(BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
+    return !state.getValue(BlockStateProperties.WATERLOGGED) && fluid == Fluids.WATER;
+  }
+
+  @Override
+  public boolean placeLiquid(
+      LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
+    if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
+      if (!worldIn.isClientSide()) {
+        worldIn.setBlock(
+            pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
+        worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn));
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public ItemStack pickupBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+    if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+      worldIn.setBlock(
+          pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
+
+      if (!state.canSurvive(worldIn, pos)) {
+        worldIn.destroyBlock(pos, true);
+      }
+      return new ItemStack(Items.WATER_BUCKET);
+    } else {
+      return ItemStack.EMPTY;
+    }
+  }
+
+  @Override
+  public Optional<SoundEvent> getPickupSound() {
+    return Fluids.WATER.getPickupSound();
+  }
+
+  /* Update BlockState. */
+  @SuppressWarnings("deprecation")
+  public BlockState updateShape(
+      BlockState state,
+      Direction facing,
+      BlockState facingState,
+      LevelAccessor worldIn,
+      BlockPos pos,
+      BlockPos facingPos) {
+    if (state.getValue(WATERLOGGED)) {
+      worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+    }
+
+    return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
+  }
+
+  /* Create Blockstate */
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    builder.add(AXIS, WATERLOGGED);
+  }
+
+  /* Flammable Block */
+  @Override
+  public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction direct) {
+    return true;
+  }
+
+  @Override
+  public int getFireSpreadSpeed(
+      BlockState state, BlockGetter world, BlockPos pos, Direction direct) {
+    return 5;
+  }
+
+  @Override
+  public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction direct) {
+    return 20;
+  }
 }
