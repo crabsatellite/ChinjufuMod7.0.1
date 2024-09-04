726f6a3a23c1fcdbaae8c595af46495e203ac85d
Alex Lee <islicy@outlook.com>
2024-09-04 11:22:25 -0400

    Use old Base_WakeWater

--- src/main/java/com/ayutaki/chinjufumod/blocks/cmblock/Base_WakeWater.java
+++ src/main/java/com/ayutaki/chinjufumod/blocks/cmblock/Base_WakeWater.java
@@ -1,12 +1,8 @@
 package com.ayutaki.chinjufumod.blocks.cmblock;
 
-import java.util.Optional;
-
-import javax.annotation.Nullable;
-
 import com.ayutaki.chinjufumod.handler.CMEvents;
 import com.ayutaki.chinjufumod.registry.Items_Teatime;
-
+import java.util.Optional;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.InteractionHand;
@@ -32,86 +28,104 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class Base_WakeWater extends Block implements BucketPickup {
 
-	protected static final VoxelShape AABB_BOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 0.1D, 16.0D);
+  protected static final VoxelShape AABB_BOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 0.1D, 16.0D);
 
-	public Base_WakeWater(BlockBehaviour.Properties properties) {
-		super(properties);
-	}
+  public Base_WakeWater(BlockBehaviour.Properties properties) {
+    super(properties);
+  }
 
-	/* RightClick Action*/
-	@Override
-	public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player playerIn, InteractionHand hand, BlockHitResult hit) {
+  /* RightClick Action*/
+  @Override
+  public InteractionResult use(
+      BlockState state,
+      Level worldIn,
+      BlockPos pos,
+      Player playerIn,
+      InteractionHand hand,
+      BlockHitResult hit) {
 
-		ItemStack stack = playerIn.getItemInHand(hand);
-		Item item = stack.getItem();
+    ItemStack stack = playerIn.getItemInHand(hand);
+    Item item = stack.getItem();
 
-		/** 水を汲む **/
-		if (item == Items.GLASS_BOTTLE) {
-			/** Collect with an Item **/
-			CMEvents.Consume1Item_Bottle(worldIn, pos, playerIn, hand);
+    /** 水を汲む * */
+    if (item == Items.GLASS_BOTTLE) {
+      /** Collect with an Item * */
+      CMEvents.Consume1Item_Bottle(worldIn, pos, playerIn, hand);
 
-			ItemStack stack4 = PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.WATER);
-			if (stack.isEmpty()) { playerIn.setItemInHand(hand, stack4); }
-			else if (!playerIn.getInventory().add(stack4)) {
-				playerIn.drop(stack4, false); }
-		}
+      ItemStack stack4 = PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.WATER);
+      if (stack.isEmpty()) {
+        playerIn.setItemInHand(hand, stack4);
+      } else if (!playerIn.getInventory().add(stack4)) {
+        playerIn.drop(stack4, false);
+      }
+    }
 
-		/** TTimeItems **/
-		if (item == Items_Teatime.MIZUOKE.get()) {
-			CMEvents.Consume1Item_Bowl(worldIn, pos, playerIn, hand);
+    /** TTimeItems * */
+    if (item == Items_Teatime.MIZUOKE.get()) {
+      CMEvents.Consume1Item_Bowl(worldIn, pos, playerIn, hand);
 
-			if (stack.isEmpty()) { playerIn.getInventory().add(new ItemStack(Items_Teatime.MIZUOKE_full.get())); }
-			else if (!playerIn.getInventory().add(new ItemStack(Items_Teatime.MIZUOKE_full.get()))) {
-				playerIn.drop(new ItemStack(Items_Teatime.MIZUOKE_full.get()), false); }
-		}
+      if (stack.isEmpty()) {
+        playerIn.getInventory().add(new ItemStack(Items_Teatime.MIZUOKE_full.get()));
+      } else if (!playerIn.getInventory().add(new ItemStack(Items_Teatime.MIZUOKE_full.get()))) {
+        playerIn.drop(new ItemStack(Items_Teatime.MIZUOKE_full.get()), false);
+      }
+    }
 
-		if (item == Items_Teatime.NABE_kara.get()) {
-			CMEvents.Consume1Item_Bowl(worldIn, pos, playerIn, hand);
+    if (item == Items_Teatime.NABE_kara.get()) {
+      CMEvents.Consume1Item_Bowl(worldIn, pos, playerIn, hand);
 
-			if (stack.isEmpty()) { playerIn.getInventory().add(new ItemStack(Items_Teatime.NABESHIO_nama.get())); }
-			else if (!playerIn.getInventory().add(new ItemStack(Items_Teatime.NABESHIO_nama.get()))) {
-				playerIn.drop(new ItemStack(Items_Teatime.NABESHIO_nama.get()), false); }
-		}
+      if (stack.isEmpty()) {
+        playerIn.getInventory().add(new ItemStack(Items_Teatime.NABESHIO_nama.get()));
+      } else if (!playerIn.getInventory().add(new ItemStack(Items_Teatime.NABESHIO_nama.get()))) {
+        playerIn.drop(new ItemStack(Items_Teatime.NABESHIO_nama.get()), false);
+      }
+    }
 
-		if (item == Items_Teatime.KEIRYO_CUP.get()) {
-			CMEvents.Consume1Item_Bottle(worldIn, pos, playerIn, hand);
+    if (item == Items_Teatime.KEIRYO_CUP.get()) {
+      CMEvents.Consume1Item_Bottle(worldIn, pos, playerIn, hand);
 
-			if (stack.isEmpty()) { playerIn.getInventory().add(new ItemStack(Items_Teatime.KEIRYO_CUP_full.get())); }
-			else if (!playerIn.getInventory().add(new ItemStack(Items_Teatime.KEIRYO_CUP_full.get()))) {
-				playerIn.drop(new ItemStack(Items_Teatime.KEIRYO_CUP_full.get()), false); }
-		}
-		
-		/** SUCCESS to not put anything on top. **/
-		return InteractionResult.SUCCESS;
-	}
+      if (stack.isEmpty()) {
+        playerIn.getInventory().add(new ItemStack(Items_Teatime.KEIRYO_CUP_full.get()));
+      } else if (!playerIn.getInventory().add(new ItemStack(Items_Teatime.KEIRYO_CUP_full.get()))) {
+        playerIn.drop(new ItemStack(Items_Teatime.KEIRYO_CUP_full.get()), false);
+      }
+    }
 
-	/* Collisions for each property. */
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-		return AABB_BOX;
-	}
+    /** SUCCESS to not put anything on top. * */
+    return InteractionResult.SUCCESS;
+  }
 
-	/* Clone Item in Creative. */
-	@Override
-	public ItemStack getCloneItemStack(BlockGetter worldIn, BlockPos pos, BlockState state) {
-		return new ItemStack(Items.AIR);
-	}
+  /* Collisions for each property. */
+  @Override
+  public VoxelShape getShape(
+      BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+    return AABB_BOX;
+  }
 
-	/* BucketPickup Items.BUCKET -> Items.WATER_BUCKET */
-	@Override
-	public ItemStack pickupBlock(@Nullable Player playerIn, LevelAccessor worldIn, BlockPos pos, BlockState state) {
-		if (state.getValue(BlockStateProperties.WATERLOGGED)) {
-			worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
-			
-			if (!state.canSurvive(worldIn, pos)) { worldIn.destroyBlock(pos, true); }
-			return new ItemStack(Items.WATER_BUCKET);
-		}
-		
-		else { return ItemStack.EMPTY; }
-	} // fix 20.2
-	
-	@Override
-	public Optional<SoundEvent> getPickupSound() {
-		return Fluids.WATER.getPickupSound();
-	}
+  /* Clone Item in Creative. */
+  @Override
+  public ItemStack getCloneItemStack(BlockGetter worldIn, BlockPos pos, BlockState state) {
+    return new ItemStack(Items.AIR);
+  }
+
+  /* Items.BUCKET など Fluids.WATER の有るアイテムで水を汲む */
+  @Override
+  public ItemStack pickupBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+    if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+      worldIn.setBlock(
+          pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
+
+      if (!state.canSurvive(worldIn, pos)) {
+        worldIn.destroyBlock(pos, true);
+      }
+      return new ItemStack(Items.WATER_BUCKET);
+    } else {
+      return ItemStack.EMPTY;
+    }
+  }
+
+  @Override
+  public Optional<SoundEvent> getPickupSound() {
+    return Fluids.WATER.getPickupSound();
+  }
 }
