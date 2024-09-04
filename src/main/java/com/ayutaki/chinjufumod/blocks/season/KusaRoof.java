4130f085656376d58c942aaef676039dfdc600b7
Alex Lee <islicy@outlook.com>
2024-09-04 14:13:02 -0400

    Update KusaRoof for 1.20.1

--- src/main/java/com/ayutaki/chinjufumod/blocks/season/KusaRoof.java
+++ src/main/java/com/ayutaki/chinjufumod/blocks/season/KusaRoof.java
@@ -1,13 +1,10 @@
 package com.ayutaki.chinjufumod.blocks.season;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.annotation.Nullable;
-
 import com.ayutaki.chinjufumod.handler.CMEvents;
 import com.ayutaki.chinjufumod.registry.Items_Seasonal;
-
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.Nullable;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -50,194 +47,248 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class KusaRoof extends Block implements SimpleWaterloggedBlock {
 
-	/* Property */
-	public static final DirectionProperty H_FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
-	public static final EnumProperty<SlabType> TYPE = EnumProperty.create("type", SlabType.class);
-	public static final BooleanProperty COR = BooleanProperty.create("cor");
-	public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
+  /* Property */
+  public static final DirectionProperty H_FACING =
+      DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
+  public static final EnumProperty<SlabType> TYPE = EnumProperty.create("type", SlabType.class);
+  public static final BooleanProperty COR = BooleanProperty.create("cor");
+  public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
 
-	/* Collision */
-	protected static final VoxelShape AABB_BOTTOM = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
-	protected static final VoxelShape AABB_TOP = Block.box(0.0D, 8.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-	
-	public KusaRoof(BlockBehaviour.Properties properties) {
-		super(properties);
-		/** Default state **/
-		registerDefaultState(this.stateDefinition.any().setValue(H_FACING, Direction.NORTH)
-				.setValue(COR, Boolean.valueOf(false))
-				.setValue(TYPE, SlabType.BOTTOM)
-				.setValue(WATERLOGGED, Boolean.valueOf(false)));
-	}
+  /* Collision */
+  protected static final VoxelShape AABB_BOTTOM = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+  protected static final VoxelShape AABB_TOP = Block.box(0.0D, 8.0D, 0.0D, 16.0D, 16.0D, 16.0D);
 
-	/* RightClick Action*/
-	@Override
-	public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player playerIn, InteractionHand hand, BlockHitResult hit) {
-		ItemStack stack = playerIn.getItemInHand(hand);
-		Item item = stack.getItem();
+  public KusaRoof(BlockBehaviour.Properties properties) {
+    super(properties);
+    /** Default state * */
+    registerDefaultState(
+        this.stateDefinition
+            .any()
+            .setValue(H_FACING, Direction.NORTH)
+            .setValue(COR, Boolean.valueOf(false))
+            .setValue(TYPE, SlabType.BOTTOM)
+            .setValue(WATERLOGGED, Boolean.valueOf(false)));
+  }
 
-		if (stack.isEmpty() && item != Items_Seasonal.KUSATABA_RF.get() && 
-				item != Items_Seasonal.WARATABA_RF.get() && item != Items_Seasonal.KAYATABA_RF.get()) {
-			
-			if (playerIn.isCrouching()) {
-				worldIn.playSound(null, pos, SoundEvents.GRASS_PLACE, SoundSource.BLOCKS, 1.0F, 0.8F);
-				worldIn.setBlock(pos, state.cycle(COR), 3);
-				return InteractionResult.SUCCESS; }
-		
-			if (!playerIn.isCrouching()) {
-				CMEvents.textNotSneak(worldIn, pos, playerIn);
-				return InteractionResult.SUCCESS; }
-		}
-		
-		return InteractionResult.PASS;
-	}
-	
-	/* Gives a value when placed. +180 .getOpposite() */
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		Level worldIn = context.getLevel();
-		BlockPos pos = context.getClickedPos();
-		BlockState state = worldIn.getBlockState(pos);
-		FluidState fluid = worldIn.getFluidState(pos);
-		
-		if (state.is(this)) {
-			/** Change to SlabType.DOUBLE. **/
-			return state.setValue(TYPE, SlabType.DOUBLE); }
+  /* RightClick Action*/
+  @Override
+  public InteractionResult use(
+      BlockState state,
+      Level worldIn,
+      BlockPos pos,
+      Player playerIn,
+      InteractionHand hand,
+      BlockHitResult hit) {
 
-		else {
-			BlockState blockState2 = this.defaultBlockState().setValue(TYPE, SlabType.BOTTOM)
-					.setValue(WATERLOGGED, Boolean.valueOf(Boolean.valueOf(fluid.getType() == Fluids.WATER)))
-					.setValue(H_FACING, context.getHorizontalDirection().getOpposite());
-			Direction direction = context.getClickedFace();
+    ItemStack stack = playerIn.getItemInHand(hand);
+    Item item = stack.getItem();
 
-			return direction != Direction.DOWN && (direction == Direction.UP || context.getClickLocation().y - (double)pos.getY() <= 0.5D) ? blockState2 : blockState2.setValue(TYPE, SlabType.TOP);
-		}
-	}
+    if (stack.isEmpty()
+        && item != Items_Seasonal.KUSATABA_RF.get()
+        && item != Items_Seasonal.WARATABA_RF.get()
+        && item != Items_Seasonal.KAYATABA_RF.get()) {
 
-	/* Replace to DOUBLE. boolean t/f */
-	@Override
-	public boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
-		ItemStack stack = context.getItemInHand();
-		SlabType slabType = state.getValue(TYPE);
+      if (playerIn.isCrouching()) {
+        worldIn.playSound(null, pos, SoundEvents.GRASS_PLACE, SoundSource.BLOCKS, 1.0F, 0.8F);
+        worldIn.setBlock(pos, state.cycle(COR), 3);
+        return InteractionResult.SUCCESS;
+      }
 
-		/** DOUBLE でない時 かつ これをアイテム(アイテムブロック)として使う時 **/
-		if (slabType != SlabType.DOUBLE && stack.getItem() == this.asItem()) {
+      if (!playerIn.isCrouching()) {
+        CMEvents.textNotSneak(worldIn, pos, playerIn);
+        return InteractionResult.SUCCESS;
+      }
+    }
 
-			if (context.replacingClickedOnBlock()) {
-				boolean flag = context.getClickLocation().y - (double)context.getClickedPos().getY() > 0.5D;
-				Direction direction = context.getClickedFace();
+    return InteractionResult.PASS;
+  }
 
-				if (slabType == SlabType.BOTTOM) { return direction == Direction.UP || flag && direction.getAxis().isHorizontal(); }
+  /* Gives a value when placed. +180 .getOpposite() */
+  @Override
+  public BlockState getStateForPlacement(BlockPlaceContext context) {
+    Level worldIn = context.getLevel();
+    BlockPos pos = context.getClickedPos();
+    BlockState state = worldIn.getBlockState(pos);
+    FluidState fluid = worldIn.getFluidState(pos);
 
-				else { return direction == Direction.DOWN || !flag && direction.getAxis().isHorizontal(); }
-			}
-			else { return true; }
-		}
-		else { return false; }
-	}
+    if (state.is(this)) {
+      /** Change to SlabType.DOUBLE. * */
+      return state.setValue(TYPE, SlabType.DOUBLE);
+    } else {
+      BlockState blockState2 =
+          this.defaultBlockState()
+              .setValue(TYPE, SlabType.BOTTOM)
+              .setValue(
+                  WATERLOGGED, Boolean.valueOf(Boolean.valueOf(fluid.getType() == Fluids.WATER)))
+              .setValue(H_FACING, context.getHorizontalDirection().getOpposite());
+      Direction direction = context.getClickedFace();
 
-	/* HORIZONTAL Property */
-	@Override
-	public BlockState rotate(BlockState state, Rotation rotation) {
-		return state.setValue(H_FACING, rotation.rotate(state.getValue(H_FACING)));
-	}
+      return direction != Direction.DOWN
+              && (direction == Direction.UP
+                  || context.getClickLocation().y - (double) pos.getY() <= 0.5D)
+          ? blockState2
+          : blockState2.setValue(TYPE, SlabType.TOP);
+    }
+  }
 
-	@SuppressWarnings("deprecation")
-	public BlockState mirror(BlockState state, Mirror mirror) {
-		return state.rotate(mirror.getRotation(state.getValue(H_FACING)));
-	}
+  /* Replace to DOUBLE. boolean t/f */
+  @Override
+  public boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
+    ItemStack stack = context.getItemInHand();
+    SlabType slabType = state.getValue(TYPE);
 
-	/* Waterlogged */
-	@SuppressWarnings("deprecation")
-	public FluidState getFluidState(BlockState state) {
-		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-	
-	@Override
-	public boolean placeLiquid(LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
-		return state.getValue(TYPE) != SlabType.DOUBLE ? SimpleWaterloggedBlock.super.placeLiquid(worldIn, pos, state, fluid) : false;
-	}
+    /** DOUBLE でない時 かつ これをアイテム(アイテムブロック)として使う時 * */
+    if (slabType != SlabType.DOUBLE && stack.getItem() == this.asItem()) {
 
-	@Override
-	public boolean canPlaceLiquid(@Nullable Player playerIn, BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
-		return state.getValue(TYPE) != SlabType.DOUBLE ? SimpleWaterloggedBlock.super.canPlaceLiquid(playerIn, worldIn, pos, state, fluid) : false;
-	} // fix 20.2
-	
-	@Override
-	public ItemStack pickupBlock(@Nullable Player playerIn, LevelAccessor worldIn, BlockPos pos, BlockState state) {
-		if (state.getValue(BlockStateProperties.WATERLOGGED)) {
-			worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
-			
-			if (!state.canSurvive(worldIn, pos)) { worldIn.destroyBlock(pos, true); }
-			return new ItemStack(Items.WATER_BUCKET);
-		}
-		
-		else { return ItemStack.EMPTY; }
-	} // fix 20.2
-	
-	@Override
-	public Optional<SoundEvent> getPickupSound() {
-		return Fluids.WATER.getPickupSound();
-	}
+      if (context.replacingClickedOnBlock()) {
+        boolean flag =
+            context.getClickLocation().y - (double) context.getClickedPos().getY() > 0.5D;
+        Direction direction = context.getClickedFace();
 
-	/* Update BlockState. */
-	@SuppressWarnings("deprecation")
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor worldIn, BlockPos pos, BlockPos facingPos) {
-		if (state.getValue(WATERLOGGED)) {
-			worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn)); }
-		
-		return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
-	}
+        if (slabType == SlabType.BOTTOM) {
+          return direction == Direction.UP || flag && direction.getAxis().isHorizontal();
+        } else {
+          return direction == Direction.DOWN || !flag && direction.getAxis().isHorizontal();
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
 
-	@Override
-	public boolean isPathfindable(BlockState state, BlockGetter worldIn, BlockPos pos, PathComputationType type) {
-		switch (type) {
-		case LAND:
-			return false;
-		case WATER:
-			return worldIn.getFluidState(pos).is(FluidTags.WATER);
-		case AIR:
-			return false;
-		default:
-			return false;
-		}
-	}
+  /* HORIZONTAL Property */
+  @Override
+  public BlockState rotate(BlockState state, Rotation rotation) {
+    return state.setValue(H_FACING, rotation.rotate(state.getValue(H_FACING)));
+  }
 
-	/* Create Blockstate */
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(COR, H_FACING, TYPE, WATERLOGGED);
-	}
+  @SuppressWarnings("deprecation")
+  public BlockState mirror(BlockState state, Mirror mirror) {
+    return state.rotate(mirror.getRotation(state.getValue(H_FACING)));
+  }
 
-	@Override
-	public boolean useShapeForLightOcclusion(BlockState state) {
-		return state.getValue(TYPE) != SlabType.DOUBLE;
-	}
+  /* Waterlogged */
+  @SuppressWarnings("deprecation")
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+  }
 
-	/* Collisions for each property. */
-	public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-		SlabType slabType = state.getValue(TYPE);
-		switch (slabType) {
-		case DOUBLE:
-			return Shapes.block();
-		case TOP:
-			return AABB_TOP;
-		default:
-			return AABB_BOTTOM;
-		}
-	}
+  @Override
+  public boolean placeLiquid(
+      LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
+    return state.getValue(TYPE) != SlabType.DOUBLE
+        ? SimpleWaterloggedBlock.super.placeLiquid(worldIn, pos, state, fluid)
+        : false;
+  }
 
-	/* Flammable Block */
-	@Override
-	public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction direct) { return true; }
+  @Override
+  public boolean canPlaceLiquid(BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
+    return state.getValue(TYPE) != SlabType.DOUBLE
+        ? SimpleWaterloggedBlock.super.canPlaceLiquid(worldIn, pos, state, fluid)
+        : false;
+  }
 
-	@Override
-	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction direct) { return 5; }
+  @Override
+  public ItemStack pickupBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+    if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+      worldIn.setBlock(
+          pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
 
-	@Override
-	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction direct) { return 20; }
-	
-	/* ToolTip */
-	public void appendHoverText(ItemStack stack, @Nullable BlockGetter worldIn, List<Component> tooltip, TooltipFlag tipFlag) {
-		tooltip.add(Component.translatable("tips.block_tabakusa_roof").withStyle(ChatFormatting.GRAY));
-	}
+      if (!state.canSurvive(worldIn, pos)) {
+        worldIn.destroyBlock(pos, true);
+      }
+      return new ItemStack(Items.WATER_BUCKET);
+    } else {
+      return ItemStack.EMPTY;
+    }
+  }
+
+  @Override
+  public Optional<SoundEvent> getPickupSound() {
+    return Fluids.WATER.getPickupSound();
+  }
+
+  /* Update BlockState. */
+  @SuppressWarnings("deprecation")
+  public BlockState updateShape(
+      BlockState state,
+      Direction facing,
+      BlockState facingState,
+      LevelAccessor worldIn,
+      BlockPos pos,
+      BlockPos facingPos) {
+    if (state.getValue(WATERLOGGED)) {
+      worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+    }
+
+    return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
+  }
+
+  @Override
+  public boolean isPathfindable(
+      BlockState state, BlockGetter worldIn, BlockPos pos, PathComputationType type) {
+    switch (type) {
+      case LAND:
+        return false;
+      case WATER:
+        return worldIn.getFluidState(pos).is(FluidTags.WATER);
+      case AIR:
+        return false;
+      default:
+        return false;
+    }
+  }
+
+  /* Create Blockstate */
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    builder.add(COR, H_FACING, TYPE, WATERLOGGED);
+  }
+
+  @Override
+  public boolean useShapeForLightOcclusion(BlockState state) {
+    return state.getValue(TYPE) != SlabType.DOUBLE;
+  }
+
+  /* Collisions for each property. */
+  public VoxelShape getShape(
+      BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+    SlabType slabType = state.getValue(TYPE);
+    switch (slabType) {
+      case DOUBLE:
+        return Shapes.block();
+      case TOP:
+        return AABB_TOP;
+      default:
+        return AABB_BOTTOM;
+    }
+  }
+
+  /* Flammable Block */
+  @Override
+  public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction direct) {
+    return true;
+  }
+
+  @Override
+  public int getFireSpreadSpeed(
+      BlockState state, BlockGetter world, BlockPos pos, Direction direct) {
+    return 5;
+  }
+
+  @Override
+  public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction direct) {
+    return 20;
+  }
+
+  /* ToolTip */
+  public void appendHoverText(
+      ItemStack stack,
+      @Nullable BlockGetter worldIn,
+      List<Component> tooltip,
+      TooltipFlag tipFlag) {
+    tooltip.add(Component.translatable("tips.block_tabakusa_roof").withStyle(ChatFormatting.GRAY));
+  }
 }
