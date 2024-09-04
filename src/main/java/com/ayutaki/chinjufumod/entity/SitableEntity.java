8c4cf1fcb72dc1558fab84889a47119aab7c1acd
Alex Lee <islicy@outlook.com>
2024-09-04 14:17:53 -0400

    Update SitableEntity for 1.20.1

--- src/main/java/com/ayutaki/chinjufumod/entity/SitableEntity.java
+++ src/main/java/com/ayutaki/chinjufumod/entity/SitableEntity.java
@@ -1,9 +1,7 @@
 package com.ayutaki.chinjufumod.entity;
 
-import java.util.List;
-
 import com.ayutaki.chinjufumod.handler.EntityTypes_CM;
-
+import java.util.List;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
@@ -21,84 +19,95 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 public class SitableEntity extends Entity {
-	
-	/* Used in 'register'. */
-	public SitableEntity(EntityType<SitableEntity> type, Level worldIn) {
-		super(type, worldIn);
-	}
-	
-	public SitableEntity(Level worldIn) {
-		super(EntityTypes_CM.SITABLE.get(), worldIn);
-		this.noPhysics = true;
-	}
 
-	/* Used in 'create'. */
-	private SitableEntity(Level worldIn, BlockPos pos, double yOffset) {
-		this(worldIn);
-		this.setPos(pos.getX() + 0.5D, pos.getY() + 0.23D + yOffset, pos.getZ() + 0.5D);
-	}
+  /* Used in 'register'. */
+  public SitableEntity(EntityType<SitableEntity> type, Level worldIn) {
+    super(type, worldIn);
+  }
 
-	@SuppressWarnings("resource")
-	@Override
-	public void tick() {
-		super.tick();
+  public SitableEntity(Level worldIn) {
+    super(EntityTypes_CM.SITABLE.get(), worldIn);
+    this.noPhysics = true;
+  }
 
-		if (!this.level().isClientSide) {
-			if (this.getPassengers().isEmpty() || this.level().isEmptyBlock(this.blockPosition())) {
-				this.remove(RemovalReason.DISCARDED);
-				this.level().updateNeighbourForOutputSignal(blockPosition(), this.level().getBlockState(blockPosition()).getBlock());
-			}
-		}
-	}
+  /* Used in 'create'. */
+  private SitableEntity(Level worldIn, BlockPos pos, double yOffset) {
+    this(worldIn);
+    this.setPos(pos.getX() + 0.5D, pos.getY() + 0.23D + yOffset, pos.getZ() + 0.5D);
+  }
 
-	@Override
-	protected void defineSynchedData() {}
+  @SuppressWarnings("resource")
+  @Override
+  public void tick() {
+    super.tick();
 
-	@Override
-	protected void readAdditionalSaveData(CompoundTag compound) {}
+    if (!this.level().isClientSide) {
+      if (this.getPassengers().isEmpty() || this.level().isEmptyBlock(this.blockPosition())) {
+        this.remove(RemovalReason.DISCARDED);
+        this.level()
+            .updateNeighbourForOutputSignal(
+                blockPosition(), this.level().getBlockState(blockPosition()).getBlock());
+      }
+    }
+  }
 
-	@Override
-	protected void addAdditionalSaveData(CompoundTag compound) {}
+  @Override
+  protected void defineSynchedData() {}
 
-	@Override
-	protected float ridingOffset(Entity entity) {
-		return 0.0F;
-	} // fix 20.2
+  @Override
+  protected void readAdditionalSaveData(CompoundTag compound) {}
 
-	@Override
-	protected boolean canRide(Entity entity) {
-		return true;
-	}
+  @Override
+  protected void addAdditionalSaveData(CompoundTag compound) {}
 
-	@Override
-	public Packet<ClientGamePacketListener> getAddEntityPacket() {
-		return new ClientboundAddEntityPacket(this);
-	} // fix 20.2
+  @Override
+  protected boolean canRide(Entity entity) {
+    return true;
+  }
 
-	public static InteractionResult create(Level worldIn, BlockPos pos, double yOffset, Player player) {
-		if(!worldIn.isClientSide()) {
-			List<SitableEntity> seats = worldIn.getEntitiesOfClass(SitableEntity.class, new AABB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1.0, pos.getY() + 1.0, pos.getZ() + 1.0));
-			
-			if(seats.isEmpty()) {
-				SitableEntity seat = new SitableEntity(worldIn, pos, yOffset);
-				worldIn.addFreshEntity(seat);
-				player.startRiding(seat, false);
-			}
-		}
-		return InteractionResult.SUCCESS;
-	}
-	
-	/* Position when stopping the ride. */
-	@Override
-	public Vec3 getDismountLocationForPassenger(LivingEntity entity) {
-		Direction original = this.getMotionDirection();
-		Direction[] offsets = {original, original.getClockWise(), original.getCounterClockWise(), original.getOpposite()};
-		for(Direction dir : offsets) {
-			Vec3 safeVec = DismountHelper.findSafeDismountLocation(entity.getType(), this.level(), this.blockPosition().relative(dir), false);
-			if(safeVec != null) {
-				return safeVec.add(0.0D, 0.25D, 0.0D);
-			}
-		}
-		return super.getDismountLocationForPassenger(entity);
-	}
+  @Override
+  public Packet<ClientGamePacketListener> getAddEntityPacket() {
+    return new ClientboundAddEntityPacket(this);
+  } // fix 20.2
+
+  public static InteractionResult create(
+      Level worldIn, BlockPos pos, double yOffset, Player player) {
+    if (!worldIn.isClientSide()) {
+      List<SitableEntity> seats =
+          worldIn.getEntitiesOfClass(
+              SitableEntity.class,
+              new AABB(
+                  pos.getX(),
+                  pos.getY(),
+                  pos.getZ(),
+                  pos.getX() + 1.0,
+                  pos.getY() + 1.0,
+                  pos.getZ() + 1.0));
+
+      if (seats.isEmpty()) {
+        SitableEntity seat = new SitableEntity(worldIn, pos, yOffset);
+        worldIn.addFreshEntity(seat);
+        player.startRiding(seat, false);
+      }
+    }
+    return InteractionResult.SUCCESS;
+  }
+
+  /* Position when stopping the ride. */
+  @Override
+  public Vec3 getDismountLocationForPassenger(LivingEntity entity) {
+    Direction original = this.getMotionDirection();
+    Direction[] offsets = {
+      original, original.getClockWise(), original.getCounterClockWise(), original.getOpposite()
+    };
+    for (Direction dir : offsets) {
+      Vec3 safeVec =
+          DismountHelper.findSafeDismountLocation(
+              entity.getType(), this.level(), this.blockPosition().relative(dir), false);
+      if (safeVec != null) {
+        return safeVec.add(0.0D, 0.25D, 0.0D);
+      }
+    }
+    return super.getDismountLocationForPassenger(entity);
+  }
 }
