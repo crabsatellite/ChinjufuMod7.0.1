2e05f3c2708bd28c0cbe8b1f6b73e3bcf9fde63f
Alex Lee <islicy@outlook.com>
2024-09-04 14:07:10 -0400

    Update BaseTaru_Yoh and Makibishi for 1.20.1

--- src/main/java/com/ayutaki/chinjufumod/blocks/garden/Makibishi.java
+++ src/main/java/com/ayutaki/chinjufumod/blocks/garden/Makibishi.java
@@ -1,14 +1,11 @@
 package com.ayutaki.chinjufumod.blocks.garden;
 
+import com.ayutaki.chinjufumod.handler.CMEvents;
+import com.ayutaki.chinjufumod.registry.Items_Wadeco;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
-
 import javax.annotation.Nullable;
-
-import com.ayutaki.chinjufumod.handler.CMEvents;
-import com.ayutaki.chinjufumod.registry.Items_Wadeco;
-
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -48,124 +45,159 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class Makibishi extends FallingBlock implements SimpleWaterloggedBlock {
 
-	/* Property */
-	public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
-	/* Collision */
-	protected static final VoxelShape AABB_BOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
+  /* Property */
+  public static final BooleanProperty WATERLOGGED = BooleanProperty.create("waterlogged");
+  /* Collision */
+  protected static final VoxelShape AABB_BOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
 
-	public Makibishi(BlockBehaviour.Properties properties) {
-		super(properties);
-		/** Default state **/
-		registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.valueOf(false)));
-	}
+  public Makibishi(BlockBehaviour.Properties properties) {
+    super(properties);
+    /** Default state * */
+    registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.valueOf(false)));
+  }
 
-	/* RightClick Action */
-	@Override
-	public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player playerIn, InteractionHand hand, BlockHitResult hit) {
-		CMEvents.soundTouchBlock(worldIn, pos);
-		/** SUCCESS to not put anything on top. **/
-		return InteractionResult.SUCCESS;
-	}
-	
-	/* Gives a value when placed. +180 .getOpposite() */
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		Level worldIn = context.getLevel();
-		BlockPos pos = context.getClickedPos();
-		FluidState fluid = worldIn.getFluidState(pos);
+  /* RightClick Action */
+  @Override
+  public InteractionResult use(
+      BlockState state,
+      Level worldIn,
+      BlockPos pos,
+      Player playerIn,
+      InteractionHand hand,
+      BlockHitResult hit) {
+    CMEvents.soundTouchBlock(worldIn, pos);
+    /** SUCCESS to not put anything on top. * */
+    return InteractionResult.SUCCESS;
+  }
 
-		return this.defaultBlockState().setValue(WATERLOGGED, Boolean.valueOf(fluid.getType() == Fluids.WATER));
-	}
-	
-	/* Waterlogged */
-	@SuppressWarnings("deprecation")
-	public FluidState getFluidState(BlockState state) {
-		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-	
-	@Override
-	public boolean canPlaceLiquid(@Nullable Player playerIn, BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
-		return fluid == Fluids.WATER;
-	} // fix 20.2
-	
-	@Override
-	public boolean placeLiquid(LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
-		if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
-			if (!worldIn.isClientSide()) {
-				worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
-				worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn)); }
-			return true; }
-		
-		else { return false; }
-	}
-	
-	@Override
-	public ItemStack pickupBlock(@Nullable Player playerIn, LevelAccessor worldIn, BlockPos pos, BlockState state) {
-		if (state.getValue(BlockStateProperties.WATERLOGGED)) {
-			worldIn.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
-			
-			if (!state.canSurvive(worldIn, pos)) { worldIn.destroyBlock(pos, true); }
-			return new ItemStack(Items.WATER_BUCKET);
-		}
-		
-		else { return ItemStack.EMPTY; }
-	} // fix 20.2
-	
-	@Override
-	public Optional<SoundEvent> getPickupSound() {
-		return Fluids.WATER.getPickupSound();
-	}
-	
-	/* Update BlockState. */
-	@Override
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor worldIn, BlockPos pos, BlockPos facingPos) {
-		if ((Boolean)state.getValue(WATERLOGGED)) {
-			worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn)); }
-		
-		worldIn.scheduleTick(pos, this, this.getDelayAfterPlace());
-		
-		return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
-	}
+  /* Gives a value when placed. +180 .getOpposite() */
+  @Override
+  public BlockState getStateForPlacement(BlockPlaceContext context) {
+    Level worldIn = context.getLevel();
+    BlockPos pos = context.getClickedPos();
+    FluidState fluid = worldIn.getFluidState(pos);
 
-	/* Create Blockstate */
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(WATERLOGGED);
-	}
+    return this.defaultBlockState()
+        .setValue(WATERLOGGED, Boolean.valueOf(fluid.getType() == Fluids.WATER));
+  }
 
-	/* Collisions for each property. */
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-		return AABB_BOX;
-	}
+  /* Waterlogged */
+  @SuppressWarnings("deprecation")
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+  }
 
-	/* 上を歩くとダメージを受ける CactusBlock */
-	public void entityInside(BlockState state, Level worldIn, BlockPos pos, Entity entityIn) {
-		Random rand = new Random();
-		
-		if (entityIn instanceof LivingEntity) {
-			/** ネコ、ヤマネコ、ニワトリ、オウム、ゴーレムを除外 **/
-			if (entityIn instanceof Cat || entityIn instanceof Ocelot || entityIn instanceof Parrot || entityIn instanceof IronGolem) { }
-			
-			if (entityIn instanceof Chicken && rand.nextInt(60) == 0) {
-				entityIn.hurt(worldIn.damageSources().generic(), 0.5F); }
-			
-			if (!(entityIn instanceof Cat) && !(entityIn instanceof Ocelot) && !(entityIn instanceof Parrot) &&
-					!(entityIn instanceof IronGolem) && !(entityIn instanceof Chicken)) {
-				entityIn.hurt(worldIn.damageSources().generic(), 0.5F); } }
-		
-		if (!(entityIn instanceof LivingEntity)) { }
-	}
-	
-	/* Clone Item in Creative. */
-	@Override
-	public ItemStack getCloneItemStack(BlockGetter worldIn, BlockPos pos, BlockState state) {
-		return new ItemStack(Items_Wadeco.MAKIBISHI.get());
-	}
-	
-	/* ToolTip */
-	@Override
-	public void appendHoverText(ItemStack stack, @Nullable BlockGetter worldIn, List<Component> tooltip, TooltipFlag tipFlag) {
-		tooltip.add(Component.translatable("tips.block_makibishi").withStyle(ChatFormatting.GRAY));
-	}
+  @Override
+  public boolean canPlaceLiquid(BlockGetter worldIn, BlockPos pos, BlockState state, Fluid fluid) {
+    return !state.getValue(BlockStateProperties.WATERLOGGED) && fluid == Fluids.WATER;
+  }
+
+  @Override
+  public boolean placeLiquid(
+      LevelAccessor worldIn, BlockPos pos, BlockState state, FluidState fluid) {
+    if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluid.getType() == Fluids.WATER) {
+      if (!worldIn.isClientSide()) {
+        worldIn.setBlock(
+            pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(true)), 3);
+        worldIn.scheduleTick(pos, fluid.getType(), fluid.getType().getTickDelay(worldIn));
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public ItemStack pickupBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+    if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+      worldIn.setBlock(
+          pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.valueOf(false)), 3);
+
+      if (!state.canSurvive(worldIn, pos)) {
+        worldIn.destroyBlock(pos, true);
+      }
+      return new ItemStack(Items.WATER_BUCKET);
+    } else {
+      return ItemStack.EMPTY;
+    }
+  }
+
+  @Override
+  public Optional<SoundEvent> getPickupSound() {
+    return Fluids.WATER.getPickupSound();
+  }
+
+  /* Update BlockState. */
+  @Override
+  public BlockState updateShape(
+      BlockState state,
+      Direction facing,
+      BlockState facingState,
+      LevelAccessor worldIn,
+      BlockPos pos,
+      BlockPos facingPos) {
+    if ((Boolean) state.getValue(WATERLOGGED)) {
+      worldIn.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+    }
+
+    worldIn.scheduleTick(pos, this, this.getDelayAfterPlace());
+
+    return super.updateShape(state, facing, facingState, worldIn, pos, facingPos);
+  }
+
+  /* Create Blockstate */
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    builder.add(WATERLOGGED);
+  }
+
+  /* Collisions for each property. */
+  @Override
+  public VoxelShape getShape(
+      BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+    return AABB_BOX;
+  }
+
+  /* 上を歩くとダメージを受ける CactusBlock */
+  public void entityInside(BlockState state, Level worldIn, BlockPos pos, Entity entityIn) {
+    Random rand = new Random();
+
+    if (entityIn instanceof LivingEntity) {
+      /** ネコ、ヤマネコ、ニワトリ、オウム、ゴーレムを除外 * */
+      if (entityIn instanceof Cat
+          || entityIn instanceof Ocelot
+          || entityIn instanceof Parrot
+          || entityIn instanceof IronGolem) {}
+
+      if (entityIn instanceof Chicken && rand.nextInt(60) == 0) {
+        entityIn.hurt(worldIn.damageSources().generic(), 0.5F);
+      }
+
+      if (!(entityIn instanceof Cat)
+          && !(entityIn instanceof Ocelot)
+          && !(entityIn instanceof Parrot)
+          && !(entityIn instanceof IronGolem)
+          && !(entityIn instanceof Chicken)) {
+        entityIn.hurt(worldIn.damageSources().generic(), 0.5F);
+      }
+    }
+
+    if (!(entityIn instanceof LivingEntity)) {}
+  }
+
+  /* Clone Item in Creative. */
+  @Override
+  public ItemStack getCloneItemStack(BlockGetter worldIn, BlockPos pos, BlockState state) {
+    return new ItemStack(Items_Wadeco.MAKIBISHI.get());
+  }
+
+  /* ToolTip */
+  @Override
+  public void appendHoverText(
+      ItemStack stack,
+      @Nullable BlockGetter worldIn,
+      List<Component> tooltip,
+      TooltipFlag tipFlag) {
+    tooltip.add(Component.translatable("tips.block_makibishi").withStyle(ChatFormatting.GRAY));
+  }
 }
